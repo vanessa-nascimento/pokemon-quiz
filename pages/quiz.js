560ab2926fb828c6152e346ed3ca25ca94d823eb
Vanessa Nascimento <vanessa.nascimento@picpay.com>
2021-02-01 13:25:22 -0300

    Corrigindo import do  React

--- pages/quiz.js
+++ pages/quiz.js
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import React from 'react';	import React from 'react';
+import React from 'react';
 import db from '../db.json';
 import Widget from '../src/components/Widget';
 import QuizLogo from '../src/components/QuizLogo';
